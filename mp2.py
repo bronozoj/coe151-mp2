e8d7350a62da26f3ea1c643b8874a58e3e6c3d5e
Jaime Bronozo <bronozoj@gmail.com>
2017-04-18 17:25:38 +0800

    command line argument parsing

--- mp2.py
+++ mp2.py
@@ -1,7 +1,59 @@
 from socket import socket, AF_INET, SOCK_DGRAM
+from sys import argv
+
+class nodeinfo:
+	def __init__(self, ip, nexthop, cost):
+		self.travelcost = cost
+		self.destip = ip
+		self.nexthopip = nexthop
+
+	def nexthop(self):
+		return self.nexthopip
+
+	def nexthop(self, nexthop):
+		self.nexthopip = nexthop
+		return nexthop
+
+	def cost(self):
+		return self.cost
+
+	def cost(self, cost):
+		self.travelcost = cost
+		return cost
+
+interval = 10000
+psinfinity = 100
+port = 12345
+filedir = ''
+poisonenabled = False
+
+buf = ''
+for hana in argv:
+	if buf == '':
+		if hana == '-poison':
+			poisonenabled = True
+		elif hana[:1] == '-':
+			buf = hana[1:]
+	else:
+		if buf == 'port':
+			port = int(hana)
+		elif buf == 'psinf':
+			psinfinity = int(hana)
+		elif buf == 'interval':
+			interval = int(hana)
+		elif buf == 'config':
+			filedir = hana
+		buf = ''
+
+if filedir == '':
+	print('Current Settings...')
+	print('Interval: ' + str(interval) )
+	print('Current Port: ' + str(port) )
+	print('Poisoned Reverse mode: ' + str(poisonenabled) )
+	print('Pseudo-infinity value: ' + str(psinfinity) )
+	print('No file given.. exiting')
+	exit()
 
 sock = socket(AF_INET, SOCK_DGRAM)
 
 sock.close()
-
-print('hana song')
